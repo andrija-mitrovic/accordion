636b5ee9b2583148693f9d3bb369074261ccd957
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-01-30 09:36:29 +0100

    Configure app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import SingleQuestion from "./components/Question";
+import data from "./data";
+
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => (
+            <SingleQuestion key={question.id} question={question} />
+          ))}
+        </section>
+      </div>
+    </main>
   );
 }
 
